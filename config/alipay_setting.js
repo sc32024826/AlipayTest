5adb01decfe3933d84a258999b681c3fe9ae77c8
shencheng <shencheng_1014@aliyun.com>
2019-10-28 16:30:24 +0800

    一直都报错

--- config/alipay_setting.js
+++ config/alipay_setting.js
@@ -6,10 +6,9 @@ let alipayConfig = {
     APP_GATEWAY_URL: 'xxxxxxx',//用于接收支付宝异步通知
     //第三方授权或用户信息授权后回调地址。授权链接中配置的redirect_uri的值必须与此值保持一致。
     AUTH_REDIRECT_URL: 'xxxxxxx',
-    APP_PRIVATE_KEY_PATH: path.join(__dirname, 'pem', 'private.pem'),//应用私钥
-    APP_PUBLIC_KEY_PATH: path.join(__dirname, 'pem', 'public.pem'),//应用公钥
-    ALI_PUBLIC_KEY_PATH: path.join(__dirname, 'pem', 'remind', 'sandbox', 'ali-public.pem'),//阿里公钥
-    AES_PATH: path.join(__dirname, 'pem', 'remind', 'sandbox', 'aes.txt'),//aes加密（暂未使用）
+    APP_PRIVATE_KEY_PATH: path.join(__dirname, 'pem', 'APP_PRIVATE_KEY.pem'),//应用私钥
+    APP_PUBLIC_KEY_PATH: path.join(__dirname, 'pem', 'APP_PUBLIC_KEY.pem'),//应用公钥
+    ALI_PUBLIC_KEY_PATH: path.join(__dirname, 'pem', 'ALIPAY_PUBLIC_KEY.pem'),//阿里公钥
 }
 module.exports = {
     alipayConfig
--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,57 @@ const AlipaySdk = require('alipay-sdk').default;
 const AlipayFormData = require('alipay-sdk/lib/form').default
 const fs = require('fs')
 const path = require('path')
-const PRIVATE_KEY_PATH = path.join(__dirname,'../config/pem/private.pem')
-const PUBLIC_KEY_PATH = path.join(__dirname,'../config/pem/public.pem')
+const PRIVATE_KEY_PATH = path.join(__dirname, '../config/pem/APP_PRIVATE_KEY.pem')  //应用私钥
+const PUBLIC_KEY_PATH = path.join(__dirname, '../config/pem/ALIPAY_PUBLIC_KEY.pem')  //支付宝公钥
 // console.log(filepath);
 
 const PRIVATE_KEY = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8')
-const PUBLIC_KEY = fs.readFileSync(PUBLIC_KEY_PATH,'utf8')
+const PUBLIC_KEY = fs.readFileSync(PUBLIC_KEY_PATH, 'utf8')
 
 
 router.get('/pay', async (ctx) => {
-    const alipaySdk = new AlipaySdk({
-      appId: '2016101200666016',
-      privateKey: PRIVATE_KEY,
-      gateway: "https://openapi.alipaydev.com/gateway.do",
-      keyType: 'PRIVATE KEY',
-      alipayPublicKey:PUBLIC_KEY
-    });
-  
-  
-    const formData = new AlipayFormData()
-    formData.setMethod("get")
-    formData.addField("notifyUrl", "") // 回调地址必须为当前服务的线上地址！
-    formData.addField("returnUrl", "http://www.baidu.com")
-    formData.addField("bizContent", {
-      body: "测试商品",
-      subject: "女装",
-      outTradeNo: new Date().valueOf(),
-      totalAmount: "88.88",
-      productCode: "QUICK_WAP_WAY"
-    })
-    const result = await alipaySdk.exec("alipay.trade.wap.pay", {}, {
-      formData: formData,
-      validateSign: true
-    })
-    ctx.body = result
+  const alipaySdk = new AlipaySdk({
+    appId: '2016101200666016',
+    privateKey: PRIVATE_KEY,
+    gateway: "https://openapi.alipaydev.com/gateway.do",
+    keyType: 'PRIVATE KEY',
+    alipayPublicKey: PUBLIC_KEY
+  });
+
+  // 请求参数 
+  const formData = new AlipayFormData()
+  formData.setMethod("get")
+  formData.addField("notifyUrl", "http://www.lzhb.site/paycallback") // 回调地址必须为当前服务的线上地址！
+  formData.addField("returnUrl", "http://www.lzhb.site/success")
+  formData.addField("bizContent", {
+    body: "测试商品",
+    subject: "女装",
+    outTradeNo: new Date().valueOf(),
+    totalAmount: "88.88",
+    productCode: "FAST_INSTANT_TRADE_PAY"
   })
+
+  //alipay.trade.page.pay  alipay.trade.wap.pay
+  const result = await alipaySdk.exec(
+    'alipay.trade.page.pay',
+    {},
+    { formData: formData }
+  )
+
+ctx.body = result
+})
+
+router.post('paycallback', async (ctx, next) => {
+  let postData = ctx.request.body;
+  console.log("触发付款");
+  if (postData.trade_status === "TRADE_SUCCESS") {
+    let data = ctx.request.body // 订单信息
+    // ========= 由请求体内的订单信息，在这里进行数据库中订单状态的更改 ============
+    console.log("支付完成！");
+  }
+})
+router.get('/success', async (ctx, next) => {
+  ctx.body = "支付成功"
+})
 
 module.exports = router
