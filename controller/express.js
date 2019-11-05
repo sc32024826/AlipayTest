658fca41486a5b33850d11d0efd868eb502a0fe3
shencheng <shencheng_1014@aliyun.com>
2019-11-05 13:38:32 +0800

    需要确认下 url 是否正确

--- controller/express.js
+++ controller/express.js
@@ -6,6 +6,7 @@ const { base64encode } = require('nodejs-base64')
 const URL = require('url')
 const urlencode = require('urlencode')
 const JSON = require('JSON')
+const request = require('request')
 
 /**
  * 
@@ -43,13 +44,14 @@ function encrypt(data, appkey) {
 }
 
 /**
- * 
- * @param {*} ReqURL 请求地址
+ * http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json
+ * @param {*} ReqURL 请求地址 按照php demo实际请求地址为sandboxapi.kdniao.com:8080
  * @param {*} datas 
  */
 async function sendPost(ReqURL, datas) {
+
     /*
-    // http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json
+    // 
     //第一步 需要将datas 请求信息 修改成 key=value形式的字符串
     */
     // 实际请求url : sandboxapi.kdniao.com:8080
@@ -61,26 +63,28 @@ async function sendPost(ReqURL, datas) {
     })
     let re = req.replace(/.$/, '')  //删除最后一个$ 符号
     console.log(re);
+    // let myjson = JSON.stringify(datas)
 
+    
     axios({
-        url: url,
+        url: ReqURL,
         method: 'POST',
-        data: {
-            /*  key value 形式
+        data:{
+            //   key value 形式
             EBusinessID:'test1536407',
             RequestType:'1002',
             RequestData:'%7B%27OrderCode%27%3A%27%27%2C%27ShipperCode%27%3A%27YTO%27%2C%27',
             LogisticCode:'%27%3A%2712345678%27%7D',
             DataType:'2',
             DataSign:'YThhN2VjNzllZDdiMmExZTdlN2MxNThlOTg3MTgzOWQ%3D'
-            */
-            datas
+            // *
+            // datas
         },
         headers: {
             // "user-agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)",
-            'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
             // 'Content-Length': ,
-            'Connection': 'close'
+            // 'Connection': 'close'
         }
     }).then(res => {
         console.log('res:' + res);
@@ -89,7 +93,7 @@ async function sendPost(ReqURL, datas) {
         console.log('e:' + error);
 
     })
-
+    
 }
 
 module.exports = {
