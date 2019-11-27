419e4cd57752b7486c2704383377fc25100d586e
shencheng <shencheng_1014@aliyun.com>
2019-11-27 15:52:47 +0800

    sub方法中 省略收寄人信息

--- controller/index.js
+++ controller/index.js
@@ -111,30 +111,7 @@ async function sub(ctx) {
         //查询不到结果 说明未订阅
     }
 
-    //不想给快递鸟 收集个人信息，直接以虚假信息覆盖
-    let privateInfo = {
-        PayType: 1,
-        Sender: {
-            Name: "11",
-            Tel: "",
-            Mobile: "12345678911",
-            ProvinceName: "广东省",
-            CityName: "广州市",
-            ExpAreaName: "白云区",
-            Address: "紫金港"
-        },
-        Receiver: {
-            Name: "12",
-            Tel: "",
-            Mobile: "88888888888",
-            ProvinceName: "江苏省",
-            CityName: "苏州市",
-            ExpAreaName: "扬州区",
-            Address: "御龙湾"
-        }
-    };
-    // 覆盖收寄人信息
-    let req = Object.assign({ ShipperCode, LogisticCode }, privateInfo);
+   let req = await privacy.index(ShipperCode, LogisticCode);
     try {
         //开始订阅
         var res = await express.subscribe(req);
