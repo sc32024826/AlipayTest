39fc198ae0b4a2ab5274877bf4be2873a2e24e11
shencheng <shencheng_1014@aliyun.com>
2019-12-07 13:10:30 +0800

    修改 单元测试内容,删除了回调方法

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,5 @@
 const express = require('./express');
 const Model = require('../models/express');
-const config = require("../config/index");
-const { EBusinessID } = config;
-const moment = require("moment");
 const log4js = require('../utils/log4js');
 
 /**
@@ -64,79 +61,9 @@ async function search(ctx) {
                 })
             })
         }
-
-
     }
 }
-
-/**
- * 在快递鸟配置的回调地址的方法,用于接收回调 物流信息更新数据库
- * 请求方式POST,"Content-Type": "application/json;charset=utf-8"
- */
-async function callBack(ctx) {
-    // console.log(ctx.method);   POST
-    //根据快递鸟返回的物流信息物流单号,更新数据库
-    //首先获取POST的数据
-    let post_params = ctx.request.body.RequestData;
-    // console.log(typeof post_params);  string
-
-    // 获取物流单号轨迹的数组
-    let data = JSON.parse(post_params).Data;
-    log4js.info(data);
-    let res = {
-        EBusinessID: EBusinessID,
-        UpdateTime: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-        Success: true,
-        Reason: ""
-    };
-    ctx.body = res;
-    // console.log(data);  Array(N) [Object]
-    await data.forEach(async element => {
-        //根据物流单号 查询数据库 并修改相应的轨迹信息
-        let { waybill_no, exp_company_code, Traces, Success } = element;
-        // log4js.info(Traces);  //undefined
-
-        //当state = 0 时,轨迹信息返回为null 此时 不更新
-        if (Success == true) {
-            let Reason = "数据更新";
-            await Model.updateOne({ waybill_no, exp_company_code }, { Traces, Reason }, async (err, raw) => {
-                if (err) {
-                    log4js.error("更新失败:" + err)
-                } else {
-                    log4js.info("查找结果");
-                    log4js.info(raw);
-                    //没有匹配的数据  就保存数据
-                    if (raw.n == 0) {
-                        //保存数据
-                        await Model.create(element, (err, doc) => {
-                            if (err) {
-                                log4js.error("回调信息保存失败! :" + err)
-                            } else {
-                                log4js.info("单号:" + waybill_no + ",存储成功!")
-                            }
-
-                        });
-                        return
-                    }
-                    //修改成功
-                    if (raw.nModified == 1) {
-                        log4js.info("单号:" + waybill_no + ",更新成功!")
-                    }
-                    if (raw.nModified == 0) {
-                        log4js.info("单号:" + waybill_no + ",更新失败,次数失败可以忽略,造成原因可能是数据相同")
-                    }
-
-                }
-            })
-        } else {
-            log4js.info("单号:" + waybill_no + "返回数据有问题,跳过更新!")
-        }
-
-    });
-}
-
 module.exports = {
-    search,
-    callBack
+    search
 };
 
